71aa5c679cdb346df5bc54e3fe37960df17ad906
wmillers <31211860+wmillers@users.noreply.github.com>
2021-02-13 12:25:30 +0800

    u

--- max7219.py
+++ max7219.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2017-18 Richard Hull and contributors
 # See LICENSE.rst for details.
 
-import re, time, argparse, sys
+import re, time, argparse
 from luma.led_matrix.device import max7219
 from luma.core.interface.serial import spi, noop
 from luma.core.render import canvas
@@ -11,39 +11,64 @@ from luma.core.virtual import viewport
 from luma.core.legacy import text, show_message
 from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
 
+from PIL import Image, ImageDraw, ImageFont
+
 serial = spi(port=0, device=0, gpio=noop())
 device = max7219(serial, cascaded=4, block_orientation=-90,rotate=0, blocks_arranged_in_reverse_order=False)
 
-from PIL import Image, ImageDraw, ImageFont
+class loop():
+    def __init__(self, *args):
+        self.iter=args if args else [0]
+        self.i=-1
+    
+    def __next__(self):
+        self.i+=1
+        self.i%=len(self.iter)
+        return self.iter[self.i]
 
 
-def showText(device, c, forceSingle=False, speed=2, vibe=(0,0), overflow=True, contrast=10, font=ImageFont.truetype("mem.ttf", 5), fontPadding=0, mid=True, fill=1):
+def vibe_range(space, content, vibe):
+    '''
+    |SP [content] ACE|
+    '''
+    if space>=content:
+        res=list(range(-vibe, space-content+vibe+1))
+    else:
+        res=list(range(4, -(vibe+1+4), -1))
+    resR=res.copy()
+    resR.reverse()
+    return res+resR
+
+def showText(device, c, forceSingle=False, speed=30, vibe=[0,0], overflow=True, contrast=255, font=ImageFont.truetype("mem.ttf", 5), fontPadding=0, fill=1):
     d=int(device.size[0]/(font.size-(1-fontPadding)))
     isTwoRows=not forceSingle and (font.size-1)<=device.size[1]/2
-    overflow=overflow and (font.getsize(c)[0]>device.size[0] if not isTwoRows else font.getsize(c[d:][0])>device.size[0])
-    v=True
+    overflow=overflow and (font.getsize(c)[0]>device.size[0] if not isTwoRows else font.getsize(c[d:])[0]>device.size[0])
+    if overflow:
+        if not isTwoRows:
+            vibe[0]=font.getsize(c)[0]-device.size[0]
+        else:
+            vibe[1]=font.getsize(c[d:])[0]-device.size[0]
+    v=(loop(*vibe_range(device.size[0], font.getsize(c[:d] if isTwoRows else c)[0], vibe[0])), 
+    loop(*vibe_range(device.size[0], font.getsize(c[d:])[0], vibe[1])))
     device.contrast(contrast)
-    print("|"+c[:d]+"|")
-    if isTwoRows and c[d:]:
-        print("|"+c[d:][:d]+"|")
+    print("\r|"+c[:d]+"|", vibe(0), "|"+c[d:][:d]+"|" if isTwoRows and c[d:] else "", vibe[1], flush=True, end="")
     try:
         while True:
             with canvas(device) as draw:
-                draw.text((vibe[0] if v else 0, -fontPadding), c[:d].center(d if mid else 0), fill=fill, font=font)
+                draw.text((v[0].__next__(), -fontPadding), c[:d] if isTwoRows else c, fill=fill, font=font)
                 if isTwoRows and c[d:]:
-                    draw.text((0 if v else vibe[1], 4-fontPadding), c[d:].center(d if mid else 0), fill=fill, font=font)
-            if overflow:
-                c=c[1:]+c[0]
-            elif not vibe[0] and not vibe[1]:
+                    draw.text((v[1].__next__(), 4-fontPadding), c[d:], fill=fill, font=font)
+            if not vibe[0] and not vibe[1]:
                 break
-            v=not v
             time.sleep(1/speed)
     except BaseException as e:
         device.clear()
         print("\nMaid cleaned.")
         raise e
 
-showText(device, "The quick brown fox jumps over the lazy dog", True, 5)
+showText(device, "企鹅鹅鹅鹅鹅鹅鹅", font=ImageFont.truetype("SimSun-special.ttf", 9), speed=15)
+
+showText(device, "The quick brown fox jumps over the lazy dog", True)
 showText(device, "我@试", font=ImageFont.truetype("SimSun-special.ttf", 9))
 
 
@@ -76,45 +101,6 @@ class Resquest(BaseHTTPRequestHandler):
 with canvas(device) as draw:
 text(draw, (0, 0), "Te st", fill="white")
 
-
-
-from PIL import Image, ImageDraw, ImageFont
-b=Image.new("1", (32,32), "#FFFFFF")
-a=ImageDraw.Draw(b)
-f=ImageFont.truetype("mem.ttf", 4)
-a.text((1,1), "test", font=f)
-b.save("test.png")
-
-
-msg = "Slow scrolling: 测试The quick brown fox jumps over the lazy dog"
-print(msg)
-show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
-
-for _ in range(5):
-    for intensity in range(16):
-        device.contrast(intensity * 16)
-        time.sleep(0.1)
-device.contrast(0x80)
-
-print("Vertical scrolling")
-words = [
-    "Victor", "Echo", "Romeo", "Tango", "India", "Charlie", "Alpha",
-    "Lima", " ", "Sierra", "Charlie", "Romeo", "Oscar", "Lima", "Lima",
-    "India", "November", "Golf", " "
-]
-
-virtual = viewport(device, width=device.width, height=len(words) * 8)
-with canvas(virtual) as draw:
-    for i, word in enumerate(words):
-        text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
-
-for i in range(virtual.height - device.height):
-    virtual.set_position((0, i))
-    time.sleep(0.05)
-
-def clear(device):
-    with canvas(device) as draw:
-        text(draw, (0, 0), "", fill="white", font=proportional(CP437_FONT))
 
 
 def runall(serial, device):
